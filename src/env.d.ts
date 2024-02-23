24b3afc99115c76fda69d91d25db8b8c6b59dcca
nijr@citycloud.com.cn <nijr@citycloud.com.cn>
2024-02-23 16:47:23 +0800

    feat: add env update prisma

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        DATABASE_URL: string
+        REDIS_HOST: string
+        REDIS_PORT: string
+        REDIS_PASSWORD: string
+        PORT: string
+    }
+  }
